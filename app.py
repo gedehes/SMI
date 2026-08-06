03d5f20acdd4816e5aece1753a64a0483082f569
gedehes <gabriel.de.soulages@gmail.com>
2026-08-06 19:58:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
+import os
+import numpy as np
+import pandas as pd
 import streamlit as st
 import yfinance as yf
-import pandas as pd
-import numpy as np
-import os
 
 # Configuration de la page
 st.set_page_config(page_title="Scanner SMI & Breakout", layout="wide")
